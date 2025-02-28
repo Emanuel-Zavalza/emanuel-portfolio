52f8b211ddd97a1f3f3adfbb7d6efc689ee126a5
Emanuel-Zavalza <emanuelzavalza@gmail.com>
2025-02-28 12:19:26 -0800

    initial commit

---
+++ constants/data.js
@@ -0,0 +1,122 @@
+const userData = {
+  githubUsername: "Emanuel-Zavalza",
+  name: "Emanuel Zavalza",
+  designation: "Computer Engineering Student at UCLA",
+  avatarUrl: "/me.png",
+  email: "emanuelzavalza@gmail.com",
+  phone: "+1 (661)-360-2175",
+  address: "Canyon Country, CA",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a fourth year computer engineering student looking to expand my software engineering skills while uplifting Latine students in STEM",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "EE 180 DA/DW Capstone Project",
+    currentProjectUrl: "N/A",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "mroads",
+      year: "2021",
+      companyLink: "https://mroads.com",
+      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "TailwindMasterKit",
+      year: "2021",
+      companyLink: "https://tailwindmasterkit.com",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "Algochurn",
+      year: "2021",
+      companyLink: "https://algochurn.com",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Software Engineering Intern",
+      company: "Lockheed Martin",
+      year: "2022 - 2023",
+      companyLink: "https://www.google.com/search?client=safari&rls=en&q=lockheed+martin&ie=UTF-8&oe=UTF-8",
+      desc: "Worked here for two summers and a co-op in between adding feautures to VR LM's combat simulation systems to increase fidelity & functionality. Hehe one of the components was creating the code to control a semi-aquatic tank.",
+    },
+    {
+      title: "Computer Engineering Student",
+      company: "University of California, Los Angeles",
+      year: "2021 - Now",
+      companyLink: "https://samueli.ucla.edu/",
+      desc: "Balanced my interests in computer engineering and advocating for Latinx STEM students. Nobody asks this but I commuted for two years while I attended here and it was ROUGH.",
+    },
+    {
+      title: "High School Student",
+      company: "Canyon High School, Canyon Country CA",
+      year: "2017 - 2021",
+      companyLink: "https://www.canyonhighcowboys.org/",
+      desc: "Thankful to have graduated with a GPA of 4.6 and after the waves of COVID, so I was able to walk to receive my diploma :)",
+    },
+    {
+      title: "Co-Founder & Dancer",
+      company: "Ballet Folklorico Luna Tapatía",
+      year: "2013 - Now",
+      companyLink: "https://www.instagram.com/bf.lunatapatia/",
+      desc: "Created this group with peers after the original community center program was discontinued. Now, it's a prospering community with the goal of spreading Mexican culture by performing at local events for free.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1jvHekXJpg2fKGiienYrzg-aEulV27BX_/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/emanuel.zavalza/",
+    linkedin: "https://www.linkedin.com/in/emanuelzavalza/",
+    github: "https://github.com/Emanuel-Zavalza",
+  },
+};
+
+export default userData;
